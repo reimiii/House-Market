11400a5192f62c3004801ec53ed5bc611bc870ff
reimiii <imiia75775@gmail.com>
2022-03-13 08:01:00 +0700

    .env

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -90,7 +90,7 @@ function CreateListing() {
 
     if (geolocationEnabled) {
       const response = await fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyDjVaFlbht_RUtziblBaBSpkdB4Jqj3Ch0`
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODING_API_KEY}`
       );
 
       const data = await response.json();
